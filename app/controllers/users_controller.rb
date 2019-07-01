19827e19e8e1d741a74b0bbafc9c6a28bdd12059
Dana Sevcik <danasevcik@hotmail.com>
2019-07-01 11:06:23 -0400

    started login feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,19 @@
 class UsersController < ApplicationController
+
+  def create
+    byebug
+    # create user with username and password
+    @user = User.create(user_params)
+    if @user.valid?
+      render json: { user: @user }, status: :created
+    else
+      render json: { error: 'failed to create user' }, status: :not_acceptable
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:name)
+  end
+
+
 end
--- config/application.rb
+++ config/application.rb
@@ -10,8 +10,8 @@ module TicTacToeBackend
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
     config.middleware.insert_before 0, Rack::Cors do
+
       allow do
         origins '*'
         resource '*', headers: :any, methods: [:get, :post, :options]
