3a330e80d6896a85d2da33191cd5001d19a076ef
Dana Sevcik <danasevcik@hotmail.com>
2019-07-01 11:15:21 -0400

    add hometown and fun fact to user create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:name)
+    params.require(:user).permit(:name, :hometown, :fun_fact)
   end
 
 
