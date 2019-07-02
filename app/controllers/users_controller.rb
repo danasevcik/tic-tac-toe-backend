78c44dac248c8896a4e8e18e3e2689fb6563c472
Dana Sevcik <danasevcik@hotmail.com>
2019-07-02 09:57:51 -0400

    functionality for new and returning users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,16 @@ class UsersController < ApplicationController
     end
   end
 
+  def find
+    byebug
+    @user = User.find_by(name: user_params[:name])
+    if @user.valid?
+      render json: { user: @user }, status: :created
+    else
+      render json: { error: 'failed to find user' }, status: :not_acceptable
+    end
+  end
+
   def user_params
     params.require(:user).permit(:name, :hometown, :fun_fact)
   end
