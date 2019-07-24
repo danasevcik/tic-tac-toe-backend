9327c3ea41470101ff7e438cbaff8420bedec024
Dana Sevcik <danasevcik@hotmail.com>
2019-07-24 10:56:08 -0400

    remove byebug

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -2,7 +2,6 @@ class AuthController < ApplicationController
   skip_before_action :authorized, only: [:create]
 
   def create
-    byebug
     @user = User.find_by(username: user_login_params[:username])
     #User#authenticate comes from BCrypt
     if @user && @user.authenticate(user_login_params[:password])
@@ -10,7 +9,6 @@ class AuthController < ApplicationController
       token = encode_token({ user_id: @user.id })
       render json: { user: @user, jwt: token }, status: :accepted
     else
-      byebug
       render json: { message: 'Invalid username or password' }, status: :unauthorized
     end
   end
