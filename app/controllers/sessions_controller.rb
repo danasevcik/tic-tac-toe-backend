708f66a3ce4a63245ba97679c8f638ac46e68d99
Dana Sevcik <danasevcik@hotmail.com>
2019-07-25 10:21:27 -0400

    skip before action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,6 @@ class SessionsController < ApplicationController
 
   def create_easy
     # create easy game with 5 points for score
-    byebug
     @session = Session.create(user_id: session_params[:user_id], score: session_params[:score])
     render json: { session: @session }, status: :created
   end
